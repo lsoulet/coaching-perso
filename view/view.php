c9553bec88538b022e6f98ac0b2a5967fc70e8f4
Liam Soulet <liam.soulet@etu.umontpellier.fr>
2023-01-25 16:18:03 +0100

    première page

---
+++ view/view.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Titre de la page</title>
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="#">Lien 1</a></li>
+            <li><a href="#">Lien 2</a></li>
+            <li><a href="#">Lien 3</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <section>
+        <h1>Titre principal</h1>
+        <p>Contenu de la page</p>
+    </section>
+</main>
+<footer>
+    <p>Copyright © 2021 Mon Site</p>
+</footer>
+</body>
+</html>
